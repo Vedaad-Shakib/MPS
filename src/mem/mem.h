7df54c8f5e6663583a15f2c3d42c7cc845c0eba1
Vedaad-Shakib <vedaad799@gmail.com>
2015-01-06 12:53:23 -0800

    Changed the file structure to accommodate a larger project

---
+++ src/mem/mem.h
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ ** Copyright 2014-2014 Vedaad Shakib Inc.
+ *******************************************************************************/
+
+/*******************************************************************************
+ ** 
+ ** "mem.h": memory management routines
+ **
+ *******************************************************************************/
+
+/*******************************************************************************
+ * Memory allocation macros
+ *******************************************************************************
+ */
+#define memPrintNew(T,N)        printf("Allocating %ld in %s line %d\n",\
+                                       sizeof(T)*(N), __FILE__, __LINE__ )
+#ifdef MEM_ECHO
+#define memNew(T,N)             (memPrintNew(T,N),		        \
+				 (T*) malloc(sizeof(T) * N))
+#define memNewZero(T,N)         (memPrintNew(T,N),			\
+				 (T*) memset(malloc(sizeof(T)*N),0,sizeof(T)*N))
+#else
+#define memNew(T,N)             (T*) malloc(sizeof(T) * N)
+#define memNewZero(T,N)         (T*) memset(malloc(sizeof(T)*N),0,sizeof(T)*N)
+#endif
+
+#define memResize(TYPE, ARRAY, OLDSIZE, NEWSIZE, MAXSIZE, NDIMS)	\
+    if ((NEWSIZE) >= (MAXSIZE)) {					\
+        TYPE *ary;							\
+        (MAXSIZE) = 2 * (MAXSIZE) + 100;                                \
+        if ((MAXSIZE) < (NEWSIZE)) (MAXSIZE) = (NEWSIZE);		\
+        ary = memNew(TYPE, (MAXSIZE)*(NDIMS));				\
+        if ((OLDSIZE) != 0) {						\
+            memcpy(ary, ARRAY, sizeof(TYPE)*(OLDSIZE)*(NDIMS));		\
+            free(ARRAY);                                                \
+        }								\
+        ARRAY = ary;							\
+    }
+
+#define memFree(M)              free(M)
