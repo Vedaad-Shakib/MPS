d75f82b7b5030931cbf0c200e9b89f0d45aba31b
Vedaad Shakib <vedaad799@gmail.com>
2015-07-04 17:51:02 +0800

    Made animation much faster and smoother

--- dam/pyqt.py
+++ dam/pyqt.py
@@ -4,6 +4,7 @@ import types
 import re
 import sys
 import os
+import numpy
 import matplotlib.pyplot as plt
 
 from PyQt4                              import QtGui, QtCore
@@ -11,27 +12,6 @@ from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
 from matplotlib                         import pyplot as plt
 from time                               import sleep
-
-
-def findColor(i):
-    colors = ["blue", "green", "red", "cyan", "magenta", "yellow", "black", "white"]
-    for elem in i:
-        elem = str(elem)
-        if elem in colors or (len(elem) == 7 and elem[0] == "#"):
-            return elem
-    return "blue"
-
-def findSize(i):
-    for elem in i:
-        if type(elem) == types.IntType:
-            return elem
-    return 20
-
-def findMarker(i):
-    for elem in i:
-        if str(elem) in ". , o v ^ < > s p * + x D d h H | _ ":
-            return elem.strip()
-    return "o"
 
 class Window(QtGui.QDialog):
     def __init__(self, increment=10, nSteps=500, timestep=0.0004, parent=None):
@@ -59,7 +39,47 @@ class Window(QtGui.QDialog):
         self.button.clicked.connect(self.start)
 
         # the current frame
-        self.index = 0
+        self.index = self.increment
+
+        # cache the wallpoints and ghost points x and y
+        # and get initial data set at t=0
+        file = open("ghost_points.dat", "r")
+        ghostX = []
+        ghostY = []
+        for line in file:
+            tmp = [float(i) for i in line.split()]
+            ghostX.append(tmp[0])
+            ghostY.append(tmp[1])
+
+        file = open("wall_points.dat", "r")
+        wallX = []
+        wallY = []
+        for line in file:
+            tmp = [float(i) for i in line.split()]
+            wallX.append(tmp[0])
+            wallY.append(tmp[1])
+
+        file = open("mps.0.out", "r")
+        x = []
+        y = []
+        for line in file:
+            tmp = [float(i) for i in line.split()]
+            x.append(tmp[0])
+            y.append(tmp[1])
+
+        self.line1, = self.ax1.plot(x, y, 'bo', alpha=0.8)
+        self.line2, = self.ax1.plot(ghostX, ghostY, 'go', alpha=0.8)
+        self.line3, = self.ax1.plot(wallX, wallY, 'ro', alpha=0.8)
+
+        # idk why, but this works here
+        self.ax1.cla()
+
+        self.xmin, self.xmax, self.ymin, self.ymax = plt.axis()
+        
+        #self.figure.show()
+        self.figure.canvas.draw()
+
+        self.background = self.figure.canvas.copy_from_bbox(self.ax1.bbox)
 
         # set the layout
         layout = QtGui.QVBoxLayout()
@@ -75,39 +95,47 @@ class Window(QtGui.QDialog):
         self.timer.start(1)
 
     def updatePlot(self):
+        print self.index
         if self.index > self.increment*self.nSteps:
             print "asdfadf"
             self.timer.stop()
             return
         
-        data = [["ghost_points.dat", "green"], ["wall_points.dat", "red"], [("mps.%s.out") % self.index, "blue"]]
+        data = ("mps.%s.out") % self.index
         self.index += self.increment
         
         self.ax1.cla() # clear axes
         
-        for i in data:
-            color = findColor(i)
-            markersize = findSize(i)
-            marker = findMarker(i)
-            
-            m = re.match("mps.([0-9]+).out", i[0])
-            if m:
-                step = int(m.group(1))
-                title = "Time = %.4g" % (self.timestep * step)
-                self.ax1.set_title(title)
-            file = open(i[0], "r")
-            x = []
-            y = []
-            for line in file:
-                tmp = [float(j) for j in line.split()]
-                x.append(tmp[0])
-                y.append(tmp[1])
-                
-            self.ax1.scatter(x, y, c=color, s=markersize, marker=marker)
+        title = "Time = %.4g" % (self.timestep * self.index)
+        self.ax1.set_title(title)
 
-        '''self.ax1.cla()
-        self.ax1.scatter(range(10), [random.randint(1, 10) for i in range(10)])'''
-        self.canvas.draw()
+        file = open(data, "r")
+        x = []
+        y = []
+        for line in file:
+            tmp = [float(i) for i in line.split()]
+            x.append(tmp[0])
+            y.append(tmp[1])
+
+        plt.axis([self.xmin, self.xmax, self.ymin, self.ymax])
+        
+        #self.ax1.plot(x, y, 'ro')
+        self.line1.set_xdata(x)
+        self.line1.set_ydata(y)
+        self.figure.canvas.restore_region(self.background)
+        self.ax1.draw_artist(self.line1)
+        self.ax1.draw_artist(self.line2)
+        self.ax1.draw_artist(self.line3)
+        self.figure.canvas.blit(self.ax1.bbox)
+        '''
+        self.ax1.plot(numpy.arange(1422), numpy.arange(1422), 'ro')
+        self.ax1.plot(numpy.arange(1007), numpy.arange(1007), 'ro')
+        self.ax1.plot(numpy.arange(252), numpy.arange(252), 'ro')
+        '''
+        #self.ax1.plot(self.ghostX, self.ghostY, 'ro')
+        #self.ax1.plot(self.wallX, self.wallY, 'ro')
+
+        #self.canvas.draw()
 
 def mpsPlot():
     app = QtGui.QApplication(sys.argv)
